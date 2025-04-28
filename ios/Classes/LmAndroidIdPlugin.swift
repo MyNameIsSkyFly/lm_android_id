0e4aea827d66f73198e284c7ce522f4ae8bb0dd0
gy <gaoy@ezuseapp.com>
2025-04-28 10:26:02 +0800

    增加ios 获取

--- ios/Classes/LmAndroidIdPlugin.swift
+++ ios/Classes/LmAndroidIdPlugin.swift
@@ -2,18 +2,20 @@ import Flutter
 import UIKit
 
 public class LmAndroidIdPlugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "lm_android_id", binaryMessenger: registrar.messenger())
-    let instance = LmAndroidIdPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
-
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    switch call.method {
-    case "getPlatformVersion":
-      result("iOS " + UIDevice.current.systemVersion)
-    default:
-      result(FlutterMethodNotImplemented)
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let channel = FlutterMethodChannel(name: "lm_android_id", binaryMessenger: registrar.messenger())
+        let instance = LmAndroidIdPlugin()
+        registrar.addMethodCallDelegate(instance, channel: channel)
     }
-  }
+    
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        switch call.method {
+        case "getDeviceId":
+            let applicationUUID = (UIDevice.current.identifierForVendor?.uuidString)!
+            result(applicationUUID)
+        default:
+            result(FlutterMethodNotImplemented)
+        }
+    }
+ 
 }
